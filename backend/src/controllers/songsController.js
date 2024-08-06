48d2cfc983f6745b6b3b88228c3209c197da9465
squach28 <squach3443@gmail.com>
2024-08-06 10:57:20 -0700

    refresh access token thru middleware if expired

--- backend/src/controllers/songsController.js
+++ backend/src/controllers/songsController.js
@@ -3,7 +3,15 @@ import axios from "axios";
 const SPOTIFY_TOP_SONGS_URL = "https://api.spotify.com/v1/me/top/tracks";
 
 export const getTopSongs = (req, res) => {
-  const { access_token: accessToken } = req.cookies;
+  let accessToken = "";
+  let expirationDate = "";
+  if (req.newAccessToken) {
+    accessToken = req.newAccessToken;
+    expirationDate = req.newExpirationDate;
+  } else {
+    accessToken = req.cookies["access_token"];
+    expirationDate = req.cookies["token_expiration_date"];
+  }
   if (accessToken === undefined) {
     res.status(403).send("Missing access token");
   } else {
@@ -14,6 +22,8 @@ export const getTopSongs = (req, res) => {
         },
       })
       .then((result) => {
+        res.cookie("access_token", accessToken);
+        res.cookie("token_expiration_date", expirationDate);
         res.status(200).json(result.data.items);
       })
       .catch((e) => console.log("error"));
--- backend/src/middleware/tokenMiddleware.js
+++ backend/src/middleware/tokenMiddleware.js
@@ -3,43 +3,40 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const tokenMiddleware = (req, res, next) => {
+export const tokenMiddleware = async (req, res, next) => {
   const { token_expiration_date: expirationDate, refresh_token: refreshToken } =
     req.cookies;
   const SPOTIFY_REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token";
   const today = new Date();
   const expireDate = new Date(expirationDate);
   if (expireDate.getTime() <= today.getTime()) {
-    axios
-      .post(
-        SPOTIFY_REFRESH_TOKEN_URL,
-        {
-          grant_type: "refresh_token",
-          refresh_token: refreshToken,
+    const result = await axios.post(
+      SPOTIFY_REFRESH_TOKEN_URL,
+      {
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " +
+            new Buffer.from(
+              process.env.SPOTIFY_CLIENT_ID +
+                ":" +
+                process.env.SPOTIFY_CLIENT_SECRET
+            ).toString("base64"),
         },
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization:
-              "Basic " +
-              new Buffer.from(
-                process.env.SPOTIFY_CLIENT_ID +
-                  ":" +
-                  process.env.SPOTIFY_CLIENT_SECRET
-              ).toString("base64"),
-            "Cache-Control": "max-age=0",
-          },
-        }
-      )
-      .then((result) => {
-        const newExpirationDate = new Date();
-        const hourInSeconds = 60 * 60 * 1000;
-        newExpirationDate.setTime(newExpirationDate.getTime() + hourInSeconds);
-        const newAccessToken = result.data.access_token;
-        res.cookie("access_token", newAccessToken);
-        res.cookie("token_expiration_date", newExpirationDate);
-      })
-      .catch((e) => console.log("error"));
+      }
+    );
+    const newExpirationDate = new Date();
+    const hourInSeconds = 60 * 60 * 1000;
+    newExpirationDate.setTime(newExpirationDate.getTime() + hourInSeconds);
+    const newAccessToken = result.data.access_token;
+    req.newAccessToken = newAccessToken;
+    req.newExpirationDate = newExpirationDate;
+    next();
+  } else {
+    next();
   }
-  next();
 };
