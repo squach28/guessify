9d6bca6eaf38562ea68a8bf7b97eb90a33aeff62
squach28 <squach3443@gmail.com>
2024-08-08 10:16:27 -0700

    control sign up state

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,40 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
 
 const SignUpForm = () => {
+  const [signUp, setSignUp] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleFieldChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setSignUp({
+      ...signUp,
+      [name]: value,
+    });
+  };
+
   return (
     <form className="flex flex-col gap-6 mt-4 w-full">
       <div className="flex items-center gap-2">
         <label htmlFor="email">Email:</label>
         <input
           id="email"
+          name="email"
           className="border p-1 rounded-md flex-1"
           type="email"
+          onChange={handleFieldChange}
         />
       </div>
       <div className="flex items-center gap-2">
         <label htmlFor="username">Username:</label>
         <input
           id="username"
+          name="username"
           className="border p-1 rounded-md flex-1"
           type="text"
+          onChange={handleFieldChange}
         />
       </div>
       <div className="flex items-center gap-2">
         <label htmlFor="password">Password:</label>
         <input
           id="password"
+          name="password"
           className="border p-1 rounded-md flex-1"
           type="password"
+          onChange={handleFieldChange}
         />
       </div>
       <div className="flex items-center gap-2">
         <label htmlFor="confirmPassword">Confirm Password:</label>
         <input
           id="confirmPassword"
+          name="confirmPassword"
           className="border p-1 rounded-md flex-1"
           type="password"
+          onChange={handleFieldChange}
         />
       </div>
       <button className="bg-black text-white px-1 py-2 rounded-lg">
