09d59bc96b776ce10e422c1fe700d1025ec7d6a1
squach28 <squach3443@gmail.com>
2024-08-07 20:07:46 -0700

    separate guess list item into separate GuessItem component

--- frontend/src/components/GuessesList.jsx
+++ frontend/src/components/GuessesList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const GuessesList = ({ guesses, selected, placeGuess, swap }) => {
+const GuessItem = ({ guess, placeGuess, swap, selected, index }) => {
   const getArtists = (artists) => {
     const res = artists.map((artist) => {
       return artist.name;
@@ -9,36 +9,48 @@ const GuessesList = ({ guesses, selected, placeGuess, swap }) => {
     return res.join(", ");
   };
   return (
+    <li
+      key={guess.id}
+      id={index}
+      className={`border border-black p-1 w-[90%] md:max-w-lg mx-auto text-sm rounded-md hover:cursor-pointer 
+    ${
+      selected
+        ? guess.value
+          ? "border-black"
+          : "border-blue-800 shadow-md"
+        : ""
+    }
+  ${swap && swap.id === answer.id ? "border-orange-500" : ""}
+  ${
+    guess.correct !== null
+      ? guess.correct
+        ? "border-green-500"
+        : "border-red-500"
+      : "border-black"
+  }
+  `}
+      onClick={(e) => placeGuess(e, guess)}
+    >
+      {guess.value
+        ? `${guess.value.name} - ${getArtists(guess.value.artists)}`
+        : ""}
+    </li>
+  );
+};
+
+const GuessesList = ({ guesses, selected, placeGuess, swap }) => {
+  return (
     <ol
       className={`w-full list-decimal list-inside flex flex-col justify-between gap-6`}
     >
       {guesses.map((guess, index) => (
-        <li
-          key={guess.id}
-          id={index}
-          className={`border border-black p-1 w-[90%] md:max-w-lg mx-auto text-sm rounded-md hover:cursor-pointer 
-            ${
-              selected
-                ? guess.value
-                  ? "border-black"
-                  : "border-blue-800 shadow-md"
-                : ""
-            }
-          ${swap && swap.id === answer.id ? "border-orange-500" : ""}
-          ${
-            guess.correct !== null
-              ? guess.correct
-                ? "border-green-500"
-                : "border-red-500"
-              : "border-black"
-          }
-          `}
-          onClick={(e) => placeGuess(e, guess)}
-        >
-          {guess.value
-            ? `${guess.value.name} - ${getArtists(guess.value.artists)}`
-            : ""}
-        </li>
+        <GuessItem
+          guess={guess}
+          placeGuess={placeGuess}
+          swap={swap}
+          selected={selected}
+          index={index}
+        />
       ))}
     </ol>
   );
