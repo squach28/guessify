fe790177f31e3494c56f916dc300b45603afb5ff
squach28 <squach3443@gmail.com>
2024-08-09 13:10:33 -0700

    temporarily click username to log user out

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Navbar = ({ hideAuth }) => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -13,13 +14,27 @@ const Navbar = ({ hideAuth }) => {
       .then((res) => setUser(res.data))
       .catch((e) => setUser(null));
   }, []);
+
+  const handleLogout = () => {
+    axios
+      .post(
+        `${import.meta.env.VITE_API_URL}/auth/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then(() => {
+        navigate("/");
+      });
+  };
   return (
     <ul className="flex justify-between p-4 bg-black text-white">
       <li>
         <Link to="/home">Guessify</Link>
       </li>
       {hideAuth ? null : user ? (
-        <p>{user.username}</p>
+        <p onClick={handleLogout}>{user.username}</p>
       ) : (
         <div className="flex items-center gap-4">
           <li>
