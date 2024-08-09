f28bbf182ccee2c62f66155133608bf9c2fd3ce1
squach28 <squach3443@gmail.com>
2024-08-09 12:50:43 -0700

    chagne queries to select from users table, not auth

--- backend/src/utils/queries.js
+++ backend/src/utils/queries.js
@@ -1,8 +1,8 @@
 export const queries = {
   signup:
     "INSERT INTO auth (email, username, password) VALUES  ($1, $2, $3) RETURNING id",
-  getUserByUsername: "SELECT * FROM auth WHERE username = $1",
-  getUserById: "SELECT * FROM auth WHERE id = $1",
-  getUserByEmail: "SELECT * FROM auth WHERE email = $1",
+  getUserByUsername: "SELECT * FROM users WHERE username = $1",
+  getUserById: "SELECT * FROM users WHERE id = $1",
+  getUserByEmail: "SELECT * FROM users WHERE email = $1",
   createUser: "INSERT INTO users (id, username, email) VALUES ($1, $2, $3)",
 };
