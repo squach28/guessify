b3602f52fcf516b05a485d94f230223cb6005a50
squach28 <squach3443@gmail.com>
2024-08-09 15:44:21 -0700

    display whether user connected their spotify account or not

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 const Home = () => {
+  const [connected, setConnected] = useState(false);
+  useEffect(() => {
+    isConnectedWithSpotify().then((res) => setConnected(res.connected));
+  }, []);
+
+  const isConnectedWithSpotify = async () => {
+    return axios
+      .get(`${import.meta.env.VITE_API_URL}/auth/spotify/connected`, {
+        withCredentials: true,
+      })
+      .then((res) => res.data);
+  };
   return (
     <div>
       <Navbar />
+      {connected ? (
+        <p>You already connected your account, nice!</p>
+      ) : (
+        <button>Connect your account</button>
+      )}
     </div>
   );
 };
