a2cc350af221d337da1d0dde4742ea325de2531b
squach28 <squach3443@gmail.com>
2024-08-27 11:02:22 -0700

    create basic profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,14 +1,52 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Profile = () => {
   const { id } = useParams();
-  console.log(id);
-  useEffect(() => {}, []);
+  const { data, isLoading, error } = useFetch(
+    `${import.meta.env.VITE_API_URL}/users/user/me`
+  );
+
+  console.log(data);
+
   return (
-    <div>
+    <div className="min-h-screen">
       <Navbar />
+      <div className="h-full flex flex-col items-center mt-8">
+        {isLoading ? <p>Loading...</p> : null}
+        {data ? (
+          <div className="h-full flex flex-col gap-4">
+            <img
+              width={100}
+              height={100}
+              className="mx-auto"
+              src={data.imageUrl}
+              alt="profile picture"
+            />
+            <div>
+              <span className="font-bold">Username: </span>
+              <span>{data.username}</span>
+            </div>
+            <div>
+              <span className="font-bold">Email: </span>
+              <span>{data.email}</span>
+            </div>
+            <div>
+              <span className="font-bold">Connected with Spotify: </span>
+              {data.spotifyId ? (
+                <span>Connected!</span>
+              ) : (
+                <span>Not connected yet</span>
+              )}
+            </div>
+            <button className="w-full bg-red-500 text-white font-bold px-1 py-2 rounded-md">
+              Delete account
+            </button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
