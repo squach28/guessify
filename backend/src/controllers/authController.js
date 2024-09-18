ca724ebf590a123566c8220f494ccb8d71de29d4
squach28 <squach3443@gmail.com>
2024-09-18 15:09:38 -0700

    add domain field to clear cookie

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -303,9 +303,11 @@ const getAccessTokenWithRefreshToken = async (refreshToken) => {
 };
 
 export const logOut = (req, res) => {
-  res.clearCookie("access_token");
-  res.clearCookie("spotify_refresh_token");
-  res.clearCookie("spotify_token_expiration_date");
-  res.clearCookie("spotify_access_token");
+  res.clearCookie("access_token", { domain: process.env.DOMAIN });
+  res.clearCookie("spotify_refresh_token", { domain: process.env.DOMAIN });
+  res.clearCookie("spotify_token_expiration_date", {
+    domain: process.env.DOMAIN,
+  });
+  res.clearCookie("spotify_access_token", { domain: process.env.DOMAIN });
   res.status(200).json({ message: "Successfully logged out" });
 };
